f342b1f5eb7837a50c21f7fc9f45e4b72868a016
Adesuwa Osagie <osagie.developer@gmail.com>
2021-11-27 04:54:37 -0500

    Adding About Section to homepage

--- js/hompage.js
+++ js/hompage.js
@@ -1,7 +1,34 @@
-// HIGHLIGHT NAV LINK
-$(".navLink").click(function () {
-    $(".navLink").not(this).removeClass("active");
-    $(this).addClass("active");
-    
-});
+$(document).ready(function() { 
 
+    // HIGHLIGHT NAV LINK
+    $(".navLink").click(function () {
+        $(".navLink").not(this).removeClass("active");
+        $(this).addClass("active");
+        
+    });
+
+    // GET A DIV TO APPEAR ON SCROLL
+    // Resource: http://jsfiddle.net/mohammadAdil/DFeqH/
+    // Resource: http://jsfiddle.net/mohammadAdil/DFeqH/
+    
+
+    $(window).scroll(function() {
+
+        $(".toFadeIn").each (function(i) {
+
+            let bottom_of_object = $(this).position().top + $(this).outerHeight();
+            let bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if (bottom_of_window > bottom_of_object) {
+                $(this).animate(
+                    {
+                        "opacity":'1'
+                    }, 500
+                );
+
+                console.log("hello world")
+            }
+        });
+    });
+
+});
--- js/jquery-functions.js
+++ js/jquery-functions.js
@@ -1,15 +1,18 @@
+$(document).ready(function() { 
 
-const namePic = document.querySelector(".name_pic");
-    
-// HIGHLIGHT CURRENT IMAGE
-$(".image").click(function () {
-    $(".image").not(this).removeClass("highlight");
-    $(this).addClass("highlight");
-    $(this).addClass("overlay")
-});
+    const namePic = document.querySelector(".name_pic");
+        
+    // HIGHLIGHT CURRENT IMAGE
+    $(".image").click(function () {
+        $(".image").not(this).removeClass("highlight");
+        $(this).addClass("highlight");
+        $(this).addClass("overlay")
+    });
 
-// GET NAME OF PIC TO APPEAR
+    // GET NAME OF PIC TO APPEAR
 
-$(".image").click(function() {
-    namePic.textContent = $(this).siblings().text();
+    $(".image").click(function() {
+        namePic.textContent = $(this).siblings().text();
+    });
+
 });
