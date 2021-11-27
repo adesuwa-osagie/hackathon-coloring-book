21af5307f622ee14f81011ae55efd146415b6a4e
Adesuwa Osagie <osagie.developer@gmail.com>
2021-11-27 18:32:49 -0500

    Added icons to navLinks

--- js/hompage.js
+++ js/hompage.js
@@ -31,4 +31,18 @@ $(document).ready(function() {
         });
     });
 
+    // SLIDE LEFT TO RIGHT ON SCROLL
+
+    $(window).scroll(function() {
+        $(".footer-header").each (function(i) {
+
+            let bottom_of_object = $(this).position().top + $(this).outerHeight(); 
+            let bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if (bottom_of_window > bottom_of_object) {
+                $(this).addClass("transformRight");
+            }
+        });
+    });
+
 });
